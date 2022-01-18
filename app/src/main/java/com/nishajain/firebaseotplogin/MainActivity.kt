8a3745270cf76a0aa4fda8993b2d38d43ea4f8c0
Nisha Jain <nj241098@gmail.com>
2022-01-18 18:28:03 +0530

    Initial commit

---
+++ app/src/main/java/com/nishajain/firebaseotplogin/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.nishajain.firebaseotplogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.*
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+    var number: String = ""
+
+    // create instance of firebase auth
+    lateinit var auth: FirebaseAuth
+
+    // we will use this to match the sent otp from firebase
+    lateinit var storedVerificationId: String
+    lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val sendOtp = findViewById<Button>(R.id.button)
+
+        auth = FirebaseAuth.getInstance()
+
+        // start verification on click of the button
+        sendOtp.setOnClickListener {
+            login()
+        }
+
+        // Callback function for Phone Auth
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            // This method is called when the verification is completed
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+                finish()
+                Log.d("Msg", "onVerificationCompleted Success")
+            }
+
+            // Called when verification is failed add log statement to see the exception
+            override fun onVerificationFailed(e: FirebaseException) {
+                Log.d("MSg", "onVerificationFailed  $e")
+            }
+
+            // On code is sent by the firebase this method is called
+            // in here we start a new activity where user can enter the OTP
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                Log.d("Msg", "onCodeSent: $verificationId")
+                storedVerificationId = verificationId
+                resendToken = token
+
+                // Start a new activity using intent
+                // also send the storedVerificationId using intent
+                // we will use this id to send the otp back to firebase
+                val intent = Intent(applicationContext, OTPActivity::class.java)
+                intent.putExtra("storedVerificationId", storedVerificationId)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
+    private fun login() {
+        number = findViewById<EditText>(R.id.mobileNumber).text.trim().toString()
+
+        // get the phone number from edit text and append the country cde with it
+        if (number.isNotEmpty()) {
+            number = "+91$number"
+            sendVerificationCode(number)
+        } else {
+            Toast.makeText(this, "Enter mobile number", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // this method sends the verification code
+    // and starts the callback of verification
+    // which is implemented above in onCreate
+    private fun sendVerificationCode(number: String) {
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(number) // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(this) // Activity (for callback binding)
+            .setCallbacks(callbacks) // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+        Log.d("Msg", "Auth started")
+    }
+}
